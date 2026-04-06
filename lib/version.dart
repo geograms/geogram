b888f38ccdd24158a618c2df74a6474ee77fd6c8
Max Brito <maxbrito@pm.me>
2026-04-06 10:00:43 +0200

    Bump version to 1.38.0-beta.1

--- lib/version.dart
+++ lib/version.dart
@@ -1,6 +1,6 @@
 /// App version - auto-generated from pubspec.yaml
 /// Run: dart run tool/update_version.dart
 /// This file is automatically updated by the pre-commit hook
-const String appVersion = '1.37.2';
+const String appVersion = '1.38.0-beta.1';
 const String appBuildNumber = '1';
 const String appFullVersion = '$appVersion+$appBuildNumber';
